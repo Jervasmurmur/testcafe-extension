db31435bf7d49873d6edf13f672842f22ff98df1
Englund <anton95@live.se>
2023-04-28 12:31:28 +0200

    Har separerat working folder och input. Har även lagt till exten funktion på input. Börjat med att formatera input.

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,11 @@ import * as fs from 'fs';
 import axios from 'axios';
 
 
+// TESTING
+function wait(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 class graphicalDOM {
     readonly element;
     readonly dataAttr;
@@ -71,15 +76,151 @@ function parseCheerio(selector:cheerio.Root, selectElement:string, attribut:stri
     return pageModelElements;
 }
 
+function handleInputs() {
+    return new Promise( (resolve, reject) => {
+        let input = vscode.window.showInputBox().then( (input)=>{
+            return input;
+        } );
+
+        if (input !== undefined) {
+            return resolve(input);
+        } else {
+            return reject("TOM!")
+        } 
+    })
+}
+
+function validInput(value:string) {
+    if (value.trim().length === 0) {
+        return "Input cannot be empty";
+    }
+    return null;
+}
+    
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerCommand('testcafe-extension.start', () => {
+    
+    let disposable = vscode.commands.registerCommand('testcafe-extension.start', async () => {
         
-        // IDÉ: Den borde hämta nuvarande Uri som defualt men om nåt annas har angivis i settings
-        //      så borde den hämta Uri ifrån setttings.
+        try {
+            if(vscode.workspace.workspaceFolders) {
+                var workingDictPath = vscode.workspace.workspaceFolders[0].uri
+                var currentPath = workingDictPath.fsPath;
+
+            } else {
+                throw new Error("Didnt find any working directory")
+            }
+        } catch (error) {
+            console.log(error)
+            return;
+        }
+        
+        var inputUrl = await vscode.window.showInputBox({
+            title: "Give an url",
+            placeHolder: "www.ssg/exempel.com",
+            prompt: "Choose a site to scan",
+            validateInput: (input) => { return validInput(input); }
+        })
+        if (inputUrl === undefined) { return; }
+        
+        var inputName = await vscode.window.showInputBox({
+            title: "Name page model",
+            placeHolder: "user input",
+            value: "test exempel", valueSelection: [-1, -1],   // TEST
+            prompt: "Give name for file and page model",
+            validateInput: (input) => { return validInput(input); }
+        })
+        if (inputName === undefined) { return; }
+
+        // user input -> user-input-page-model.ts
+        // user input -> userInputPageModel
+        // 
+        // USER input -> USER-input-page-model.ts
+        // USER-input -> USER-input-page-model.ts
+        // USER input -> userInputPageModel
+        
+        
+        // var fileName = "";
+        // for (let part of fileNameParts) {
+            //     fileName += part.toLowerCase() + "-";
+            //     fileName += part[0].toUpperCase();
+            // }
+            
+        // var fileName = "\\exempel-page-model.ts";
+
+        // NOTE: Användaren kan ange en väg med namnet, gör nånting åt?
+        var fileName = "\\" + inputName.trim().replace(/\s+/, "-") + "-page-model.ts";
+        var newFileUri = vscode.Uri.file(currentPath + fileName);
+
+        var className = "";
+        // - Gör allt till små bokstäver?
+        // - leta efter en pattern där det är whitespace innan
+        // - ta gör förta bokstaven där till storbokstav och resten till små bokstäver
+        // - check med att göra första bokstaven i strängen till liten bokstav?
+
+        // - whitespace ska tas bort
+        // - varje del mellan whitespace ska få stor bokstav
+        // - dock ska inte första bokstaven ha en stor bosktav
+
+        // const myString = 'user input';
+        // const camelCaseString = myString.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match: string, index: number) => {
+        // if (+match === 0) {
+        //     return ''; // or "-" or "_" or whatever separator you want to use
+        // }
+        // return index === 0 ? match.toLowerCase() : match.toUpperCase();
+        // });
+
+        // console.log(camelCaseString); // Output: "userInput"
+
+
+        axios
+            .get("https://devexpress.github.io/testcafe/example/")
+            .then((response) => {
+                // const $ = cheerio.load(response.data);
+                console.log(inputUrl);
+                
+                const testFile = fs.readFileSync('C:/Users/anton/Documents/kod mapp/test/testCafé testing/fancy_site.html');    // TEST
+                const $ = cheerio.load(testFile);   // TEST
+
+                var pageModelElements: graphicalDOM[] = [];
+
+                pageModelElements.push.apply(pageModelElements, parseCheerio($, "button", "data-test"));
+                pageModelElements.push.apply(pageModelElements, parseCheerio($, "input", "data-test"));
+                
+                var edit = new vscode.WorkspaceEdit();
+
+                // TODO: Behöver en bättre check om filen finns och man gör cancelled så finns det en chans att den skriver över
+                //       den nuvarande filen med ingenting.
+                edit.createFile(newFileUri, {overwrite : true, ignoreIfExists : false});
+
+                var pageModel = writePageModel(pageModelElements);
+
+                edit.insert(newFileUri, new vscode.Position(0, 0), pageModel);
+                
+
+                vscode.workspace.applyEdit( edit ).then((applyRes) =>  {
+                    if (!applyRes) { console.log("Apply failed") }  // ERROR LOG
+
+                }).then( () => {
+                    // Sparar filen
+                    vscode.workspace.openTextDocument(newFileUri).then( (doc) => {
+                        doc.save();
+                    } )
+                })
+
+            }) .catch((err) => console.log("Fetch error " + err));  // ERROR LOG
+
+        // - parse DOM-element
+        // - write/create file och namn
+        // - skriv klass
+        // - insert klass i fil
+        // - apply
+        // - Spara
+
+
+        /*
         if(vscode.workspace.workspaceFolders !== undefined) {
             
             var workingDictPath = vscode.workspace.workspaceFolders[0].uri;            
@@ -89,14 +230,19 @@ export function activate(context: vscode.ExtensionContext) {
             
             
             // TODO: Lägg till cache för förslag till input boxen
-            vscode.window.showInputBox().then( (input) => {
+            vscode.window.showInputBox({
+                prompt: "www.ssg/exempel.com",
+                validateInput: (input) => { return validInput(input); }
+            }
+            ).then( (input) => {
                 axios
                     .get("https://devexpress.github.io/testcafe/example/")
                     .then((response) => {
-                        // const $ = cheerio.load(input);
+                        const $ = cheerio.load(response.data);
+                        console.log(input);
                         
                         const testFile = fs.readFileSync('C:/Users/anton/Documents/kod mapp/test/testCafé testing/fancy_site.html');    // TEST
-                        const $ = cheerio.load(testFile);   // TEST
+                        // const $ = cheerio.load(testFile);   // TEST
 
                         var pageModelElements: graphicalDOM[] = [];
 
@@ -130,6 +276,7 @@ export function activate(context: vscode.ExtensionContext) {
         } else {
             console.log("Didnt find any working directory");
         }
+        */
 
 	});
 
